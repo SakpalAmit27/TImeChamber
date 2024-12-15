a3ba9ffaf890ea5da7a852488d22333a6ad20db5
Sakpal Amit <amitsakpal744@gmail.com>
2024-12-15 20:59:53 +0530

    tested the maxwidthwrapper works well (temporary code)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
+import { MaxWidthWrapper } from "./components/max-width-wrapper";
+
 export default function Home(){
   return(
-    <div>TimeChamber</div>
+    <section className="border-b border-red-700 relative py-24 sm:py-32 bg-brand-25">
+      <MaxWidthWrapper>
+        <div className="relative mx-auto text-center flex flex-col items-center gap-10">
+          <h1>TimeChamber</h1>
+        </div>
+      </MaxWidthWrapper>
+    </section>
   )
 }
